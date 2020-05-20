9010bbe706566c6cb2ba8a6e4ce388c3f922ba18
dingyuchen <dingyuchen@gmail.com>
2020-05-20 11:41:03 +0800

    Replace Node class with tuple

--- CS3243_P1_01_1.py
+++ CS3243_P1_01_1.py
@@ -1,33 +1,19 @@
-# CS3243 Test: No Visited Set
+# CS3243 Introduction to Artificial Intelligence
 # Project 1: k-Puzzle
 
 import os
 import sys
 import heapq
-import functools
-import copy
+from collections import deque
 
 # Running script on your own - given code can be run with the command:
 # python file.py, ./path/to/init_state.txt ./output/output.txt
 
 # Helper class to contain state information and position of blank tile, etc.
 # Contains comparator for PriorityQueue
-@functools.total_ordering
-class Node(object):
-    def __init__(self, cost, state, pos, prev, move):
-        self.cost = cost
-        self.state = state
-        self.pos = pos
-        self.prev = prev
-        self.move = move
 
-    def __lt__(self, other):
-        return self.cost < other.cost
 
-    def __eq__(self, other):
-        return self.cost == other.cost
-
-class Base(object):
+class Puzzle(object):
     def __init__(self, init_state, goal_state):
         # you may add more attributes if you think is useful
         self.init_state = init_state
@@ -37,40 +23,61 @@ class Base(object):
                         (1, 0): "UP",
                         (-1, 0): "DOWN"}
         self.n = len(init_state)
+        self.visited = set()
+        self.mapping = dict()
+        for i, row in enumerate(self.goal_state):
+            for j, v in enumerate(row):
+                self.mapping[v] = (i, j)
 
     def solve(self):
         # implement your search algorithm here
         pq = []
+        pos = 0
         for i, row in enumerate(self.init_state):
             for j, v in enumerate(row):
                 if v == 0:
-                    self.pos = (i, j)
-        heapq.heappush(pq, Node(0, self.init_state, self.pos, None, (0, 0)))
+                    pos = (i, j)
+        heapq.heappush(pq, (0, self.init_state, pos, None, (0, 0)))
 
         while pq:
             curr = heapq.heappop(pq)
-            if self.goal_test(curr.state):
+            cost, state, pos, prev, move = curr
+            self.visited.add(tuple(map(tuple, state)))
+            if self.goal_test(state):
                 return self.solution(curr)
             for move in self.actions:
                 dx, dy = move
-                x, y = curr.pos
+                x, y = pos
                 nx = x + dx
                 ny = y + dy
                 if self.is_valid(nx, ny):
-                    new_state = copy.deepcopy(curr.state)
-                    new_state[x][y] = new_state[nx][ny] 
+                    # new_state = copy.deepcopy(curr.state)
+                    new_state = [[v for v in row] for row in state]
+                    new_state[x][y] = new_state[nx][ny]
                     new_state[nx][ny] = 0
-                    new_node = Node(self.cost(curr, new_state), new_state, (nx, ny), curr, move)
+                    if tuple(map(tuple, new_state)) in self.visited:
+                        continue
+                    new_node = (self.cost(curr, new_state),
+                                new_state, (nx, ny), curr, move)
                     heapq.heappush(pq, new_node)
 
-        return ["UNSOLVABLE"] 
+        return ["UNSOLVABLE"]
 
     # you may add more functions if you think is useful
     def is_valid(self, nx, ny):
         return nx >= 0 and nx < self.n and ny < self.n and ny >= 0
 
     def cost(self, prev_node, curr_state):
-        return prev_node.cost + 1
+        return prev_node[0] + 1 + self.manhattan(curr_state)
+
+    def manhattan(self, state):
+        sum = 0
+        for i, row in enumerate(state):
+            for j, v in enumerate(row):
+                if v != 0:
+                    x, y = self.mapping[v]
+                    sum += abs(i - x) + abs(j - y)
+        return sum
 
     def goal_test(self, state):
         for i, row in enumerate(state):
@@ -80,34 +87,30 @@ class Base(object):
         return True
 
     def solution(self, node):
-        soln = []
-        curr = node
-        while curr.prev is not None:
-            soln.append(self.actions[curr.move])
-            curr = curr.prev
-        return soln[::-1]
+        soln = deque()
+        while node[3] is not None:
+            soln.appendleft(self.actions[node[4]])
+            node = node[3]
+        return list(soln)
 
+    # adapted from https://www.cs.bham.ac.uk/~mdr/teaching/modules04/java2/TilesSolvability.html
 
-# Wrapper class to inject cost function
-class Puzzle(Base):
-    def __init__(self, init_state, goal_state):
-        super(Puzzle, self).__init__(init_state, goal_state)
-        self.mapping = dict()
-        for i, row in enumerate(self.goal_state):
+    def is_solvable(self):
+        lst = []
+        zeroRow = -1
+        for i, row in enumerate(self.init_state):
             for j, v in enumerate(row):
-                self.mapping[v] = (i, j)
-
-    def cost(self, prev_node, curr_state):
-        return prev_node.cost + 1 + self.manhattan(curr_state)
-
-    def manhattan(self, state):
-        sum = 0
-        for i, row in enumerate(state):
-            for j, v in enumerate(row):
-                x, y = self.mapping[v]
-                sum += abs(i - x) + abs(j - y)
-        return sum
-
+                lst.append(v)
+                if v == 0:
+                    zeroRow = i
+        inv = 0
+        for i, t in enumerate(lst):
+            for j, v in enumerate(lst[i+1:]):
+                if v != 0 and t != 0 and v < t:
+                    inv += 1
+        width = len(self.init_state)
+        return (width % 2 == 1 and inv % 2 == 0) or (width % 2 == 0 and
+                                                     (((self.n - zeroRow + 1) % 2 == 1) == (inv % 2 == 0)))
 
 
 if __name__ == "__main__":
@@ -134,15 +137,13 @@ if __name__ == "__main__":
     # Instantiate a 2D list of size n x n
     init_state = [[0 for i in range(n)] for j in range(n)]
     goal_state = [[0 for i in range(n)] for j in range(n)]
-
-
-    i,j = 0, 0
+    i, j = 0, 0
     for line in lines:
         for number in line.split(" "):
             if number == '':
                 continue
-            value = int(number , base = 10)
-            if  0 <= value <= max_num:
+            value = int(number, base=10)
+            if 0 <= value <= max_num:
                 init_state[i][j] = value
                 j += 1
                 if j == n:
@@ -150,13 +151,12 @@ if __name__ == "__main__":
                     j = 0
 
     for i in range(1, max_num + 1):
-        goal_state[(i-1)//n][(i-1)%n] = i
+        goal_state[(i-1)//n][(i-1) % n] = i
     goal_state[n - 1][n - 1] = 0
 
     puzzle = Puzzle(init_state, goal_state)
     ans = puzzle.solve()
 
-    with open(sys.argv[2], 'a') as f:
+    with open(sys.argv[2], 'w') as f:
         for answer in ans:
             f.write(answer+'\n')
-
